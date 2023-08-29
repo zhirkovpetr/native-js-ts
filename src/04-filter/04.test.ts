40e0bffb69fbdd5ad31335a2d6ca2102b758876f
Petr <zhirkovpetr95@gmail.com>
2023-08-29 14:21:32 +0300

    refactor: refactor code filter

--- src/04-filter/04.test.ts
+++ src/04-filter/04.test.ts
@@ -1,35 +1,20 @@
+import {chipPredicate, curses, predicate} from "./04";
+
 test("should take old man, older then 91", ()=> {
     const AgePeople = [12, 22, 44, 66, 88, 99, 100, 102]
-   /* const predicate = (age: number) => {
-        return age > 91
-    }
-    const OldPeople = AgePeople.filter(predicate);*/
-    const OldPeople = AgePeople.filter(age=> age>91)
-
+    const OldPeople = AgePeople.filter(predicate);
 
     expect(OldPeople.length).toBe(3);
-
+    expect(OldPeople.length).toBe(3);
     expect(OldPeople[0]).toBe(99);
     expect(OldPeople[2]).toBe(102);
 })
 
 
 test("should take chip curses", ()=> {
-    const curses = [
-        {title: 'CSS', price: 110},
-        {title: 'JS', price: 150},
-        {title: 'React', price: 200}
-    ]
-    /*const Chippredicate = (curse: cursesType) => {
-        return curse.price < 160
-    }
-    const ChipCurses = curses.filter(Chippredicate);*/
-    const ChipCurses = curses.filter(curse=> curse.price<160)
-
-
+    const ChipCurses = curses.filter(chipPredicate);
 
     expect(ChipCurses.length).toBe(2);
-
     expect(ChipCurses[0].price).toBe(110);
     expect(ChipCurses[1].title).toBe('JS');
 })
@@ -40,14 +25,10 @@ test("get only completed tasks", ()=> {
         {id: 2, title: 'milk', isDone: false},
         {id: 3, title: 'sugar', isDone: true}
     ]
+    const completedTasks = tasks.filter(task=> task.isDone)
 
-    const complitedTasks = tasks.filter(task=> task.isDone)
-
-
-
-    expect(complitedTasks.length).toBe(1);
-
-    expect(complitedTasks[0].title).toBe('sugar');
+    expect(completedTasks.length).toBe(1);
+    expect(completedTasks[0].title).toBe('sugar');
 })
 
 test("get only uncompleted tasks", ()=> {
@@ -56,13 +37,8 @@ test("get only uncompleted tasks", ()=> {
         {id: 2, title: 'milk', isDone: false},
         {id: 3, title: 'sugar', isDone: true}
     ]
+    const uncompletedTasks = tasks.filter(task=> !task.isDone)
 
-    const uncomplitedTasks = tasks.filter(task=> !task.isDone) /*!task.isDone -идентичные записи*/
-
-
-
-    expect(uncomplitedTasks.length).toBe(2);
-
-    expect(uncomplitedTasks[0].title).toBe('bread');
+    expect(uncompletedTasks.length).toBe(2);
+    expect(uncompletedTasks[0].title).toBe('bread');
 })
-
