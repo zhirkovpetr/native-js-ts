fe7ac93220d86e26f52830accdd4a15fa5c6a874
Petr <zhirkovpetr95@gmail.com>
2023-09-07 18:26:09 +0300

    refactor: code refactor

--- src/08-associative-array/08.test.ts
+++ src/08-associative-array/08.test.ts
@@ -1,31 +1,28 @@
-
-
-
-export type usersType={
-    [ key: string]: {id: number, name: string}
+export type UsersType = {
+  [key: string]: { id: number, name: string }
 }
 
-let users:usersType
+let users: UsersType
 
-beforeEach(() =>{
-   users= {
-        '101': {id: 101, name: 'Dimych'},
-        '34576': {id: 34576, name: 'Natasha'},
-        '196295992': {id: 196295992, name: 'Valery'},
-        '1': {id: 1, name: 'Katya'}
-    }
+beforeEach(() => {
+  users = {
+    '101': {id: 101, name: 'Petr'},
+    '34576': {id: 34576, name: 'Natasha'},
+    '196295992': {id: 196295992, name: 'Valery'},
+    '1': {id: 1, name: 'Katya'}
+  }
 })
 
-test("should update corresponding user", ()=> {
-    users['1'].name= 'Ekaterina'
-    expect(users['1'].name).toBe('Ekaterina');
-    expect(users['1']).toEqual({id: 1, name: 'Ekaterina'});
+test("should update corresponding user", () => {
+  users['1'].name = 'Ekaterina'
+  expect(users['1'].name).toBe('Ekaterina');
+  expect(users['1']).toEqual({id: 1, name: 'Ekaterina'});
 
 })
 
-test("should delete corresponding user", ()=> {
-    delete  users['1']
-    expect(users['1']).toBeUndefined();
+test("should delete corresponding user", () => {
+  delete users['1']
+  expect(users['1']).toBeUndefined();
 })
 
 
